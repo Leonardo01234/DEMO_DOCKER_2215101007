dd650b2d7b496a786d37f2e1e0cfac2f9539cb2f
Leonardo Fransiskus S <128493094+Leonardo01234@users.noreply.github.com>
2024-07-02 23:06:46 +0800

    Add files via upload

---
+++ DOCKER1/web/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>DEMO DOCKER</title>
+</head>
+<body>
+    <div class="container">
+        <h1>DEMO DOCKER</h1>
+        <?php
+        echo "<p>Nama: Leonardo Fransiskus Sitanggang</p>";
+        echo "<p>NIM: 2215101007</p>";
+        echo "<p>Kelas: A</p>";
+        ?>
+    </div>
+</body>
+</html>
