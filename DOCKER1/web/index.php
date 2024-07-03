64319a3d96cec0287e892bde3265455a9397b266
Leonardo Fransiskus S <128493094+Leonardo01234@users.noreply.github.com>
2024-07-03 20:37:36 +0800

    Add files via upload

---
+++ DOCKER1/web/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>DEMO DOCKER</title>
+    <style>
+        body {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: Arial, sans-serif;
+            text-align: center;
+        }
+        .container {
+            margin-top: 20px;
+        }
+        img {
+            max-width: 100%;
+            height: auto;
+        }
+    </style>
+</head>
+<body>
+    <img src="Logo_undiksha.png" alt="Profile Picture" style="width:200px;height:200px;">
+    <div class="container">
+        <h1>DEMO DOCKER</h1>
+        <p>Nama: Leonardo Fransiskus Sitanggang</p>
+        <p>NIM: 2215101007</p>
+        <p>Kelas: A</p>
+    </div>
+</body>
+</html>
